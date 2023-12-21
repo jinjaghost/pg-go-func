08be0ce90650db212e7e3f6dec5d50d10c8b6ada
Ken Polizzi <ken.polizzi@o-kidia.com>
2023-12-21 01:42:27 +0100

    add example.go

---
+++ example.go
@@ -0,0 +1,43 @@
+package main
+
+/*
+#include "postgres.h"
+#include "utils/elog.h"
+#include "fmgr.h"
+extern void elog_error(char* string);
+
+*/
+import "C"
+
+import "fmt"
+
+var _ = fmt.Printf
+
+//
+// invoked in pg using :
+//
+// CREATE OR REPLACE FUNCTION return_true(text, text) RETURNS boolean
+// AS '$PATH/functions.so', 'returnTrue'
+// LANGUAGE C STRICT;
+//
+
+//export returnTrue
+func returnTrue(fcinfo *funcInfo) Datum {
+    // get args
+	var a string
+	var b string
+	err := fcinfo.Scan(
+		&a,
+		&b,
+	)
+
+	if err != nil {
+		C.elog_error(C.CString(
+			err.Error(),
+		))
+	}
+
+    // do job
+	return toDatum(true)
+}
+
